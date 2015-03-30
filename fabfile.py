074e3f36697cbf0db1232c59092163ccf38900aa
Max Korinets <mkorinets@gmail.com>
2015-03-30 19:31:10 +0700

    removed fixes

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,6 @@ from ZODB.FileStorage import FileStorage
 from fabric.api import *
 from fabric.colors import *
 
-from setup import VERSION
 from price_watch.models import (ProductCategory, StorageManager,
                                 ProductPackage, PriceReport,
                                 PackageLookupError, Product, Merchant,
@@ -279,8 +278,6 @@ def prepare():
         local('git describe --tags > VERSION.txt')
         local('~/env2/bin/python setup.py sdist --formats=gztar',
               capture=False)
-        local('mv dist/{}.tar.gz '
-              'dist/{}.tar.gz'.format(VERSION.replace('+', '-'), VERSION))
     else:
         print(yellow('Git: directory not clean.'))
         sys.exit()
